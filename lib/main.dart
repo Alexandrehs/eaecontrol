88652487ea5fd38c1a9c2d625430d816db92a712
TQIAlexandreHenrique <tqi_ahenrique@uolinc.com>
2024-10-03 17:07:52 -0300

    Adicionando firebase pra deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:eaecontrol/configs/app_colors.dart';
 import 'package:eaecontrol/views/scaffold_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,10 +10,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    _firebaseInit();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -22,6 +24,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const ScaffoldView(),
+    );
+  }
+
+  _firebaseInit() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
     );
   }
 }
